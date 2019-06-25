6a2629f4b07a6aa29cf8ca542f7b1af99b4f7b07
shalindra shrestha <shalindra.shrestha@payscale.com>
2019-06-25 12:47:32 -0700

    updated basename

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render((
-    <BrowserRouter basename="/build">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App/>
     </BrowserRouter>
 ), document.getElementById('root'));
